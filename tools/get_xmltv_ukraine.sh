3013c19105cbf4caf9bdf5c0276f91bf83e40fa2
George Popovich <erop1991@gmail.com>
2013-11-18 19:20:54 +0200

    Fixed tmp dir

--- tools/get_xmltv_ukraine.sh
+++ tools/get_xmltv_ukraine.sh
@@ -3,12 +3,18 @@ BASEDIR=$( cd $(dirname $0) ; pwd -P )
 URL1="http://www.teleguide.info/download/new3/tvguide.zip"
 URL2="http://www.vipiko.tv/get/?cur-xtv-xml"
 
-mkdir $TMPDIR/xmltv
-cd $TMPDIR/xmltv
+TEMP="$TMPDIR"
+
+if [ -z "$TMPDIR" ]; then
+    TEMP="/tmp"
+fi
+
+mkdir $TEMP/xmltv
+cd $TEMP/xmltv
 curl -o xml.zip $URL1
 unzip -o xml.zip
 curl -o second.xml $URL2
 tv_cat tvguide.xml second.xml > tv.xml
 mv tv.xml $BASEDIR/../static/
 cd /
-rm -rf $TMPDIR/xmltv
+rm -rf $TEMP/xmltv
