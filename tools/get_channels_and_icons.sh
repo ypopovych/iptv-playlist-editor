83dc60453c7c2c5bb1a89ee26b39178753d81757
George Popovich <erop1991@gmail.com>
2013-11-24 20:03:40 +0200

    Fixed script path

--- tools/get_channels_and_icons.sh
+++ tools/get_channels_and_icons.sh
@@ -4,9 +4,10 @@ PROJDIR=$( cd $(dirname $0)/../ ; pwd -P )
 ICONS_DIR=""
 XML="$PROJDIR/static/tv.xml"
 OUT="$PROJDIR/xmltv_channels.py"
+SCRIPT="$PROJDIR/tools/get_channels_from_xmltv.py"
 
 if [ ! -z "$ICONS_DIR" ]; then
-    python get_channels_from_xmltv.py -i "$ICONS_DIR" "$XML" "$OUT"
+    python "$SCRIPT" -i "$ICONS_DIR" "$XML" "$OUT"
 else
-    python get_channels_from_xmltv.py "$XML" "$OUT"
+    python "$SCRIPT" "$XML" "$OUT"
 fi
