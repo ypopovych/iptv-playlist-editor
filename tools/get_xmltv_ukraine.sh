38e23a9970361d6d87acc2a499f0dafc3a351498
Yegor Popovych <popovych_yegor@gmail.com>
2014-01-13 17:26:45 +0200

    Disabled tv_merge. Try to use tv_cat

--- tools/get_xmltv_ukraine.sh
+++ tools/get_xmltv_ukraine.sh
@@ -18,10 +18,10 @@ tar zxvf second.tgz
 cat xmltv.xml | tv_sort --by-channel > tg.xml
 cat tvguide.xml | tv_sort --by-channel > vp.xml
 python $BASEDIR/merge_xmltv_ids.py -i tg.xml -m vp.xml -o vp_fixed.xml
-cd $BASEDIR
-perl tv_merge -i $TEMP/xmltv/tg.xml -m $TEMP/xmltv/vp_fixed.xml -o $TEMP/xmltv/temp.xml
-cd $TEMP/xmltv
-cat temp.xml | tv_sort --by-channel  > tv.xml
+#cd $BASEDIR
+#perl tv_merge -i $TEMP/xmltv/tg.xml -m $TEMP/xmltv/vp_fixed.xml -o $TEMP/xmltv/temp.xml
+#cd $TEMP/xmltv
+tv_cat tg.xml vp_fixed.xml | tv_sort --by-channel  > tv.xml
 mv tv.xml $BASEDIR/../static/
 cd /
 rm -rf $TEMP/xmltv
